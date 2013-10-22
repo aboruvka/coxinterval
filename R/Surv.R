d743b44b6e68c70f2a874440a438db91455ef1ca
aboruvka <audrey.boruvka@gmail.com>
2013-10-22 12:33:23 -0400

    Fix Surv wrapper

--- R/Surv.R
+++ R/Surv.R
@@ -2,13 +2,14 @@
 Surv <- function(time, time2, event,
                  type = c("right", "left", "interval", "counting", "interval2",
                    'interval3'), origin = 0) {
-  if (type != 'interval3')
+  nargs <- 3 - missing(time) - missing(time2) - missing(event)
+  if (missing(type)) type <- c(rep("right", 2), "counting")[nargs]
+  if (type != "interval3")
     survival::Surv(time, time2, event, type, origin)
   else {
-    s <- survival::Surv(time, time2, 1*(event %in% c(1, 3)), type = "counting",
-                        origin)
+    s <- survival::Surv(time, time2, 1*(event %in% c(1, 3)), type, origin)
     s[, 3][event %in% 3] <- 3
-    attr(s, "type") <- 'interval3'
+    attr(s, "type") <- "interval3"
     s
   }
 }
